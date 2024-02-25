3ca5f354a1d299589a4fbcee1f7a45d825041185
Rajesh Salimath <rajeshsalimath02@gmail.com>
2024-02-25 15:20:04 +0530

    Greatest

---
+++ Greatestno.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+	int A,B;
+	
+	A=10;
+	B=5;
+	
+	if(A>B){
+		printf("The greatest number is %d",A);
+	}else if(A>B){
+		printf("The greatest number is %d",B);
+	}
+}
