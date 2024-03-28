0e26d55b79f2994f28e31915fa01a61aa24f07fd
rajeshcs04 <161193314+rajeshcs04@users.noreply.github.com>
2024-03-28 10:41:04 +0530

    while loop

---
+++ whileloop.c
@@ -0,0 +1,19 @@
+/*...............................
+          whileloop.c
+          Rajesh C S
+         24/08/2023
+................................*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+
+{
+	int i=1;
+	
+	while(i>=5){
+			printf("%d",i);
+		i++;
+	}	
+ } 
