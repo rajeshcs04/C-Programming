edfdc2be5cce32bee3476a088db04d7bf2b0e103
rajeshcs04 <161193314+rajeshcs04@users.noreply.github.com>
2024-03-28 10:42:08 +0530

    Sum math

---
+++ summath.c
@@ -0,0 +1,25 @@
+/*........................
+        Summath.c
+        Rajesh c s
+        24/08/2023
+.........................*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+	int n,i=1,S,T;
+	
+	printf("Program to find N Natural numbers\n");
+	
+	printf("Enter N:");
+	scanf("%i",&n);
+	
+	while(i<=n){
+		S+=T;
+		T+=1;
+		i++;
+	}
+	printf("S:%i",S);
+}
